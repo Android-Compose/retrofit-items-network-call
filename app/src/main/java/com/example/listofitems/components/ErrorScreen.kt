b796884f7b10c46830f71bb74137036c46410432
faballen <freezamillerstella@gmail.com>
2024-09-19 22:06:20 -0500

    removing commented code and white spaces

--- app/src/main/java/com/example/listofitems/components/ErrorScreen.kt
+++ app/src/main/java/com/example/listofitems/components/ErrorScreen.kt
@@ -1,7 +1,7 @@
 package com.example.listofitems.components
 
 
-import android.util.Log
+
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
--- app/src/main/java/com/example/listofitems/data/ItemsRepository.kt
+++ app/src/main/java/com/example/listofitems/data/ItemsRepository.kt
@@ -2,8 +2,6 @@ package com.example.listofitems.data
 
 import com.example.listofitems.model.Item
 import com.example.listofitems.network.ItemApiService
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import retrofit2.HttpException
 import java.io.IOException
 
@@ -35,31 +33,3 @@ class NetWorkItemsRepository(private val apiService : ItemApiService) : ItemsRep
         }
     }
 }
-
-
-// withContext(Dispatchers.IO) is used to perform the network call off
-// the main thread to avoid blocking the main thread which could freeze the UI
-
-//class NetWorkItemsRepository(private val apiService : ItemApiService) : ItemsRepository {
-//    override suspend fun getItems(): Result<List<Item>> {
-//        // withContext(Dispatchers.IO) is used to perform the network call off
-//        // the main thread to avoid blocking the main thread which could freeze the UI
-//        return withContext(Dispatchers.IO) {
-//            try {
-//                // response return a value create asynchronously from the apiService
-//                val response = apiService.getItems()
-//                // if successful, return the body
-//                if(response.isSuccessful) {
-//                    Result.Success(response.body() ?: emptyList())
-//                } else {
-//                    // if not successful, return a response error
-//                    Result.Error(HttpException(response))
-//                }
-//            } catch(io : IOException) {
-//                Result.Error(io) // returns network error
-//            } catch(exception: Exception) {
-//                Result.Error(exception)
-//            }
-//        }
-//    }
-//}
